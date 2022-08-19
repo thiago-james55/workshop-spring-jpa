a6693190121464c599817693f13a53bed5c16b07
Thiago Gonçalves Souza <tsyhwh55@gmail.com>
2022-08-19 19:31:56 -0300

    Exception handling - delete

--- src/main/java/com/educandoweb/course/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/educandoweb/course/resources/exceptions/ResourceExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.educandoweb.course.resources.exceptions;
 
+import com.educandoweb.course.services.exceptions.DatabaseException;
 import com.educandoweb.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.xml.crypto.Data;
 import java.time.Instant;
 
 @ControllerAdvice
@@ -17,6 +19,14 @@ public class ResourceExceptionHandler {
 
           String error = "Resource not found!";
           HttpStatus status = HttpStatus.NOT_FOUND;
+          StardardError stdError = new StardardError(Instant.now(),status.value(),error,e.getMessage(),request.getRequestURI());
+          return ResponseEntity.status(status).body(stdError);
+     }
+     @ExceptionHandler(DatabaseException.class)
+     public ResponseEntity<StardardError> database(DatabaseException e, HttpServletRequest request) {
+
+          String error = "Database Error!";
+          HttpStatus status = HttpStatus.BAD_REQUEST;
           StardardError stdError = new StardardError(Instant.now(),status.value(),error,e.getMessage(),request.getRequestURI());
           return ResponseEntity.status(status).body(stdError);
      }
